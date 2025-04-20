ba1d2f9f313193372f40dce2c0817ed7500efe49
Ayush190730 <148128641+Ayush190730@users.noreply.github.com>
2025-04-20 11:57:16 +0530

    push

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import "dotenv/config";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
 import restaurantRoute from "./routes/RestaurantRoute"
 import { v2 as cloudinary } from "cloudinary";
-import Restaurant from './models/restaurant';
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
 .connect(process.env.MONGO_URI as string)
@@ -20,9 +19,17 @@ cloudinary.config({
 
 const app  = express();
 
+// app.use(cors());
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
 // Middlewares 
 app.use(express.json());  // automatically converts the body of any request to json
-app.use(cors());
+
 
 app.get("/health", async (req: Request, res: Response) => {
           res.send({ message: "health OK!" });
@@ -31,7 +38,22 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute)
+
 
 app.listen(4000, ()=>{
           console.log('Server has started at 4000 🚀');
 })
+
+
+
+// {
+//   ...
+//   "paymentMethod": {
+//       "type": "scheme",
+//       "encryptedCardNumber": "test_5555555555554444",
+//       "encryptedExpiryMonth": "test_03",
+//       "encryptedExpiryYear": "test_2030",
+//       "encryptedSecurityCode": "test_737"
+//     }
+//   }
---
+++ src/routes/OrderRoute.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { createCheckoutSession, getMyOrders, stripeWebhookHandler} from "../controllers/OrderController";
+
+
+
+const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, getMyOrders);
+
+router.post(
+  "/checkout/create-checkout-session",
+  jwtCheck,
+  jwtParse,
+  createCheckoutSession
+);
+
+router.post("/checkout/webhook", stripeWebhookHandler);
+
+export default router;
